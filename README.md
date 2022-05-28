b8c336134c7f41e8bfebc6aeae580a2b00c5c156
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2022-05-28 18:33:04 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+
 # ponglib
+
  Something I can actually do
+
+## running
+
+Requirements; .NET 6.0
+
+Windows;
+> dotnet run Main.cs (in the src directory)
+
+Unix systems;
+idk
---
+++ src/Main.cs
@@ -0,0 +1,12 @@
+using System;
+using Raylib_cs;
+
+namespace src
+{
+    static class GameLoop
+    {
+        static void Main() {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
